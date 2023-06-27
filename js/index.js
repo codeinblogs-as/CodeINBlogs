069dd0c7a7e753b9ebabcab694252224343ce278
anshulsoni2010 <anshulsoni2022@gmail.com>
2023-06-27 17:22:03 +0530

    new feature added

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 const GroupLink = () => {
-<<<<<<< HEAD
 window.open("https://chat.whatsapp.com/Ifc2cxkRaBkJf9LUCXYzgD")
 }
 
@@ -8,7 +7,3 @@ window.addEventListener('load', function() {
     var loadingAnimation = document.getElementById('loading-animation');
     loadingAnimation.style.display = 'none';
   });
-=======
-    window.open("https://chat.whatsapp.com/Ifc2cxkRaBkJf9LUCXYzgD")
-}
->>>>>>> 0b1812089ea92b28373a19c3f4802be4c83bf1a5
