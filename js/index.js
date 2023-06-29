222894706396da73ef964c66f5ecb5b416a85a0a
anshulsoni2010 <anshulsoni2022@gmail.com>
2023-06-29 15:57:32 +0530

    new

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const GroupLink = () => {
+window.open("https://chat.whatsapp.com/Ifc2cxkRaBkJf9LUCXYzgD")
+}
+
+// loader 
+window.addEventListener('load', function() {
+    var loadingAnimation = document.getElementById('loading-animation');
+    loadingAnimation.style.display = 'none';
+  });
